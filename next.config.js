887fcab3b39d696df59668d80f8f01f228aaeba2
Heejin Choi <natsuna@Heejins-MacBook-Air.local>
2023-11-23 00:35:12 +0900

    #

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  jsconfigPaths: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': path.join(__dirname, 'src/*'),
+      public: path.join(__dirname, 'public/*'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
